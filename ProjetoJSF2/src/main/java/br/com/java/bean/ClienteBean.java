8c5b4d5b96edaa6243296fffde8b71c61b5ab62f
Marquedante <marquedante@yahoo.com.br>
2018-07-04 22:48:52 -0300

    alteracao da classe

--- ProjetoJSF2/src/main/java/br/com/java/bean/ClienteBean.java
+++ ProjetoJSF2/src/main/java/br/com/java/bean/ClienteBean.java
@@ -4,27 +4,26 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
-@ManagedBean(name="clienteBean")////////////////////////////
+@ManagedBean(name="clienteBean")// Este eh um bean do jsf
 @RequestScoped
 public class ClienteBean {
 	
 	private String nome;
+	
 	private Integer idade;
 
 	public ClienteBean() { 
-		System.out.println("----------------------------------- ClienteBean()");
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> ClienteBean()");
 	}
 	
 	@PostConstruct
-	public void init() { System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> init()"); }
-
-	public String getNome() {
-		return nome;
+	public void init() { 
+	    System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> init()"); 
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+	public String getNome() { return nome; }
+
+	public void setNome(String nome) { this.nome = nome; }
 
 	public Integer getIdade() { return idade; }
 
