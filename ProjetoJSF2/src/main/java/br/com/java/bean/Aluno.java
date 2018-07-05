1685ad3e3a28b94034d646d6b0675cad96b2aaa0
Marquedante <marquedante@yahoo.com.br>
2018-07-05 00:07:02 -0300

    alteracao

--- ProjetoJSF2/src/main/java/br/com/java/bean/Aluno.java
+++ ProjetoJSF2/src/main/java/br/com/java/bean/Aluno.java
@@ -3,9 +3,11 @@ package br.com.java.bean;
 public class Aluno { // classe Aluno.java
 
 	private String nome;
+	private String codigo;
 	
-	public Aluno(String nome){//construtor cpm parametros
+	public Aluno(String nome, Strig codigo){//construtor cpm parametros
 	   this.nome = nome;
+	   this.codogo = codigo;
 	}
 	
 	public Aluno(){//construtor sem parametros
@@ -14,5 +16,9 @@ public class Aluno { // classe Aluno.java
 	public String getNome() { return nome; }
 
 	public void setNome(String nome) { this.nome = nome; }
+	
+	public String getCodigo() { return codigo; }
+
+	public void setCodigo(String codigo) { this.codigo = codigo; }
 	
 }
