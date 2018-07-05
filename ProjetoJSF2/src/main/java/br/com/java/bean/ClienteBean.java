9c0e537af97262cdfa1152b4871a9da78c39e107
marquedante <marquedante@yahoo.com.br>
2018-07-04 21:51:31 -0300

    commit one

---
+++ ProjetoJSF2/src/main/java/br/com/java/bean/ClienteBean.java
@@ -0,0 +1,41 @@
+package br.com.java.bean;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name="clienteBean")
+@RequestScoped
+public class ClienteBean {
+	
+	private String nome;
+	private Integer idade;
+
+	public ClienteBean() {
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> ClienteBean()");
+	}
+	
+	@PostConstruct
+	public void init() {
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> init()");
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+	
+	
+
+}
